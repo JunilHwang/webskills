3e0640138c6acf3dd0d00438032036c483fc252a
junil-hwang <tjsdlf4261@naver.com>
2018-08-16 13:47:50 +0900

    csv parsing (fgetcsv 함수 사용)

--- 4_server_side/csv_parsing.php
+++ 4_server_side/csv_parsing.php
@@ -1,14 +1,15 @@
 <?php
-	$url = __DIR__."/csv/notebook.csv";
+    $url = __DIR__."/csv/notebook.csv";
 
-	function csv_parsing ($url) {
-		$arr = [];
-		$handle = fopen($url, "r");
-	    while ($data = fgetcsv($handle, 1000, ",")) $arr[] = $data;
-	    fclose($handle);
-		return $arr;
-	}
+    function csv_parsing ($url) {
+        $arr = [];
+        $handle = fopen($url, "r");
+        while ($data = fgetcsv($handle, 1000, ",")) $arr[] = $data;
+        fclose($handle);
+        return $arr;
+    }
 
-	$csv = csv_parsing($url);
-
-	print_r($csv);
+    $csv = csv_parsing($url);
+    echo "<pre>";
+    print_r($csv);
+    echo "</pre>";
