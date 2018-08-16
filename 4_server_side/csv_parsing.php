1daa9357740f16c2901eaee2b1c23905887d1230
junil-hwang <tjsdlf4261@naver.com>
2018-08-16 13:37:49 +0900

    csv parsing

--- 4_server_side/csv_parsing.php
+++ 4_server_side/csv_parsing.php
@@ -1,13 +1,15 @@
 <?php
 	$url = __DIR__."/csv/notebook.csv";
 
-	function csv2json ($url) {
+	function csv_parsing ($url) {
 		$csv = trim(file_get_contents($url));
 		$arr = explode("\n", $csv);
 		foreach ($arr as $key=>$line) {
 			$arr[$key] = explode(',', $line);
 		}
-		print_r($arr);
+		return $arr;
 	}
 
-	csv2json($url);
+	$csv = csv_parsing($url)
+
+	print_r($csv);
